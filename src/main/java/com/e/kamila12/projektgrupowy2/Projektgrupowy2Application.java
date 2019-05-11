bf6464bc291337d976680f9f3de60907d9515d2c
Kamila Kozłowska <kamillaaa@wp.pl>
2019-05-11 09:42:20 +0200

    Pierwsza wersja projektu

---
+++ src/main/java/com/e/kamila12/projektgrupowy2/Projektgrupowy2Application.java
@@ -0,0 +1,13 @@
+package com.e.kamila12.projektgrupowy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projektgrupowy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projektgrupowy2Application.class, args);
+    }
+
+}
