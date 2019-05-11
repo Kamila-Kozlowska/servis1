2dc96e3a107554f653ecb37e68c259b1ea8c0d7a
kamillaaa@wp.pl <kamillaaa@wp.pl>
2019-05-11 10:45:17 +0200

    "projekt1"

--- src/main/java/com/e/kamila12/projektgrupowy2/Projektgrupowy2Application.java
+++ src/main/java/com/e/kamila12/projektgrupowy2/Projektgrupowy2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Projektgrupowy2Application {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         SpringApplication.run(Projektgrupowy2Application.class, args);
-    }
+        }
 
-}
+        }
